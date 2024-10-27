b48c00cc3d36a84ed1abebee2b3cacc088cb3544
jaganathagithub <jaganathagithub@gmail.com>
2024-10-27 21:48:29 +0700

    Add user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,17 +9,6 @@
 </head>
 <body>
  <!-- Navbar -->
- <nav class="navbar navbar-expand-lg navbar-dark bg-primary"> <!-- Mengubah warna navbar menjadi biru -->
-<a class="navbar-brand" href="#">My Laravel Site</a>
-<div class="collapse navbar-collapse">
- <ul class="navbar-nav ml-auto">
- <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
- <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
- <li class="nav-item"><a class="nav-link" href="/portfolio">Portfolio</a></li>
-<li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
- </ul>
- </div>
- </nav>
  <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
 <a class="navbar-brand" href="#">My Laravel Site</a> 
 <div class="collapse navbar-collapse"> 
